c346a88e029c93333803847cfe72c087193e755e
Jared Brown <jaredbrown1@hotmail.com>
2021-04-01 21:47:04 -0700

    started adding id to elements

---
+++ src/machine.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const QuoteMachine = () => {
+	return (
+		<div id="quote-box">
+			<div id="text"></div>
+			<div id="author"></div>
+			<button id="new-quote"></button>
+			<a id="tweet-quote"></a>
+		</div>
+	);
+};
+
+export default QuoteMachine;
