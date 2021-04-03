e6919ba75b41954010fd434445c442d7069c068a
Jared Brown <jaredbrown1@hotmail.com>
2021-04-03 11:12:59 -0700

    random quote is displaying on each load

--- src/machine.js
+++ src/machine.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const QuoteMachine = () => {
+	const [quote, setQuote] = useState("");
+
+	useEffect(() => {
+		axios.get("https://api.quotable.io/random").then((res) => {
+			setQuote(res.data);
+		});
+	}, []);
+
 	return (
 		<div id="quote-box">
-			<div id="text"></div>
+			<div id="text">{quote.content}</div>
 			<div id="author"></div>
 			<button id="new-quote"></button>
 			<a id="tweet-quote"></a>
